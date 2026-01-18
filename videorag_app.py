51b654186e4e3496215c892734f5c0cbde62fc74
DuskoPre <63251896+DuskoPre@users.noreply.github.com>
2026-01-18 20:57:28 +0000

    Add copyright and Patreon ID to videorag_app.py
    Added copyright notice and Patreon ID to the file header.

--- videorag_app.py
+++ videorag_app.py
@@ -1,4 +1,6 @@
 """
+#  Copyright (c) 17.01.2026 [D. P.] aka duskop; after the call a day after from a Japanese IPO-agency, i'm adding my patreon ID: https://www.patreon.com/florkz_com
+#  All rights reserved.
 SotaVideoRAG v2.0: Improved Video Retrieval-Augmented Generation
 Complete implementation with all critical fixes and optimizations
 

ab2b004be0a08063e7593386495639d12faf6569
DuskoPre <63251896+DuskoPre@users.noreply.github.com>
2026-01-18 20:56:49 +0000

    Add copyright and Patreon ID to videorag_app_bkp.py
    Added copyright notice and Patreon ID to the file.

--- videorag_app_bkp.py
+++ videorag_app_bkp.py
@@ -1,4 +1,6 @@
 """
+#  Copyright (c) 17.01.2026 [D. P.] aka duskop; after the call a day after from a Japanese IPO-agency, i'm adding my patreon ID: https://www.patreon.com/florkz_com
+#  All rights reserved.
 SotaVideoRAG: Improved Video Retrieval-Augmented Generation
 Main application with all critical fixes and optimizations
 """
